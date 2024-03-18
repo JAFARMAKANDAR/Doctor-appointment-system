c995794ecfe987ab862632553f0be31e3b2d1a1f
JAFARMAKANDAR <jafarmakandar991@gmail.com>
2024-03-18 17:48:18 +0530

    doctor , patient, appointment done

--- clinic/urls.py
+++ clinic/urls.py
@@ -4,6 +4,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from management.views import *
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="home"),
@@ -12,5 +15,14 @@ urlpatterns = [
     path('register/', Register, name="ManagementRegister"),
     path('admin_login/', Login, name='login'),
     path('logout/', Logout_admin, name='logout'),
+    path('view_doctor/', View_Doctor, name='view_doctor'),
+    path('view_patient/', View_Patient, name='view_patient'),
+    path('view_appointment/', View_Appointment, name='view_appointment'),
+    path('add_doctor/', Add_Doctor, name='add_doctor'),
+    path('add_patient/', Add_Patient, name='add_patient'),
+    path('add_appointment/', Add_Appointment, name='add_appointment'),
+    path('delete_patient(?p<int:pid>)/', Delete_Patient, name='delete_patient'),
+    path('delete_appointment(?p<int:pid>)/', Delete_Appointment, name='delete_appointment'),
+    path('delete_doctor(?p<int:pid>)/', Delete_Doctor, name='delete_doctor'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- management/views.py
+++ management/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from.models import *
 
 
 # Create your views here.
@@ -43,3 +44,111 @@ def Logout_admin(request):
     return redirect('login')
 
 
+
+def Add_Doctor(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    error = None
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        contact = request.POST.get('contact')
+        special = request.POST.get('special')
+
+        try:
+            Doctor.objects.create(name=name, mobile=contact, special=special)
+            error = "no"
+        except Exception as e:
+            print(e)  # Log the specific error for debugging
+            error = "yes"
+    return render(request, 'add_doctor.html', {'error': error})
+
+
+def View_Doctor(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    doc = Doctor.objects.all()
+    d = {'doc': doc}
+    return render(request,'view_doctor.html', d)
+
+
+def Delete_Doctor(request,pid):
+    if not request.user.is_staff:
+        return redirect('login')
+    doctor = Doctor.objects.get(id=pid)
+    doctor.delete()
+    return redirect('view_doctor')
+
+
+
+def View_Patient(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    pat = Patient.objects.all()
+    d = {'pat': pat}
+    return render(request, 'view_patient.html', d)
+
+
+def Add_Patient(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    error = None
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        gender = request.POST.get('gender')
+        mobile = request.POST.get('mobile')
+        address = request.POST.get('address')
+
+        try:
+            Patient.objects.create(name=name, gender=gender, mobile=mobile, address=address)
+            error = "no"
+        except Exception as e:
+            print(e)  # Log the specific error for debugging
+            error = "yes"
+    return render(request, 'add_patient.html', {'error': error})
+
+def Delete_Patient(request,pid):
+    if not request.user.is_staff:
+        return redirect('login')
+    patient = Patient.objects.get(id=pid)
+    patient.delete()
+    return redirect('view_patient')
+
+
+
+
+def View_Appointment(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    appoint = Appointment.objects.all()
+    d = {'appoint': appoint}
+    return render(request, 'view_appointment.html', d)
+
+
+def Add_Appointment(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    error = None  # Initialize the error variable
+
+    doctor1 = Doctor.objects.all()
+    patient1 = Patient.objects.all()
+    if request.method == 'POST':
+        doctor = request.POST.get('doctor')
+        patient = request.POST.get('patient')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        doctor = Doctor.objects.filter(name=doctor).first()
+        patient = Patient.objects.filter(name=patient).first()
+        try:
+            Appointment.objects.create(doctor=doctor, patient=patient, date1=date, time1=time)
+            error = "no"
+        except Exception as e:
+            print(e)  # Log the specific error for debugging
+            error = "yes"
+    return render(request, 'add_appointment.html', {'error': error, 'doctor': doctor1, 'patient': patient1})
+
+def Delete_Appointment(request,pid):
+    if not request.user.is_staff:
+        return redirect('login')
+    appointment = Appointment.objects.get(id=pid)
+    appointment.delete()
+    return redirect('view_patient')
